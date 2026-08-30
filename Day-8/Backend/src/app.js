36ce05ec0608f017dd5f67baa9af235519e767a4
Parvez <parvezalam54802@gmail.com>
2026-08-30 10:22:21 +0530

    update-1

--- Day-8/Backend/src/app.js
+++ Day-8/Backend/src/app.js
@@ -3,8 +3,8 @@ const noteModel = require("./models/note.model");
 const cors = require("cors");
 const app = express();
 const path=require('path')
-app.use(express.json());
 app.use(cors())
+app.use(express.json());
 app.use(express.static('./public'))
 //POST
 app.post("/api/notes", async (req, res) => {
