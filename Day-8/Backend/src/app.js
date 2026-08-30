2df63ae1ddfc3b6f550ced260caa8aa635900f07
Parvez <parvezalam54802@gmail.com>
2026-08-30 09:36:54 +0530

    Day-8 done

---
+++ Day-8/Backend/src/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const noteModel = require("./models/note.model");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+//POST
+app.post("/api/notes", async (req, res) => {
+  const { title, discription } = req.body;
+  const note = await noteModel.create({
+    title,
+    discription,
+  });
+  res.status(201).json({
+    message: "Note created successfully",
+    note,
+  });
+});
+
+//GET
+app.get("/api/notes", async (req, res) => {
+  const notes = await noteModel.find();
+
+  res.status(200).json({
+    message: "Notes Fetched successfully",
+    notes,
+  });
+});
+
+//DELETE
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await noteModel.findByIdAndDelete(id);
+
+  res.status(200).json({
+    message: "Note delete successfully",
+  });
+});
+
+//PATCH
+app.patch("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title } = req.body;
+
+  await noteModel.findByIdAndUpdate(id, { title });
+
+  res.status(200).json({
+    message: "Note Update successfully",
+  });
+});
+
+module.exports = app;
---
+++ Day-8/Frontend/src/App.jsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [note, setNote] = useState([]);
+  const [editId, setEditId] = useState(null);
+  const [title, setTitle] = useState("");
+  const [discription, setDiscription] = useState("");
+
+  //   const titleRef = useRef();
+  //   const discriptionRef = useRef();
+
+  // GET
+  function fetchNote() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setNote(res.data.notes);
+    });
+  }
+
+  useEffect(() => {
+    fetchNote();
+  }, []);
+
+  // CREATE / UPDATE
+  function handelSubmit(e) {
+    e.preventDefault();
+
+    const { title, discription } = e.target.elements;
+
+    // Agar editId hai to UPDATE hoga
+    if (editId) {
+      axios
+        .patch("http://localhost:3000/api/notes/" + editId, {
+          title: title.value,
+          discription: discription.value,
+        })
+        .then(() => {
+          fetchNote();
+
+          // Form clear
+          title.value = "";
+          discription.value = "";
+
+          // Edit mode hata do
+          setEditId(null);
+        });
+
+      return;
+    }
+
+    // Agar editId nahi hai to CREATE hoga
+    axios
+      .post("http://localhost:3000/api/notes", {
+        title: title.value,
+        discription: discription.value,
+      })
+      .then(() => {
+        fetchNote();
+
+        title.value = "";
+        discription.value = "";
+      });
+  }
+
+  // DELETE
+  function deleteHandeler(noteId) {
+    axios.delete("http://localhost:3000/api/notes/" + noteId).then(() => {
+      fetchNote();
+    });
+  }
+
+  // UPDATE button
+  function updateNote(noteId) {
+    // jis card par click kiya us note ko find karo
+    const selectedNote = note.find((item) => item._id === noteId);
+
+    // form ke input me data daalo
+    // titleRef.current.value = selectedNote.title;
+    // discriptionRef.current.value = selectedNote.discription;
+    setTitle(selectedNote.title);
+    setDiscription(selectedNote.discription);
+
+    // ID save karo
+    setEditId(noteId);
+  }
+
+  return (
+    <div className="bg-zinc-800 w-full h-screen p-10">
+      <form onSubmit={handelSubmit} className="flex gap-4">
+        <input
+          //   ref={titleRef}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="border border-amber-50 bg-white"
+          name="title"
+          type="text"
+          placeholder="title"
+        />
+
+        <input
+          //   ref={discriptionRef}
+          value={discription}
+          onChange={(e) => setDiscription(e.target.value)}
+          className="border border-amber-50 bg-white"
+          name="discription"
+          type="text"
+          placeholder="discription"
+        />
+
+        <button className="px-4 font-bold py-1 bg-blue-600 rounded-md">
+          {editId ? "Update" : "Create"}
+        </button>
+      </form>
+
+      <div className="flex gap-4 mt-5">
+        {note.map((note, idx) => (
+          <div
+            key={idx}
+            className="w-60 h-40 bg-gray-400 rounded-md text-center p-5 text-white"
+          >
+            <h2 className="text-2xl">{note.title}</h2>
+
+            <p className="font-bold">{note.discription}</p>
+
+            <button
+              onClick={() => {
+                deleteHandeler(note._id);
+              }}
+              className="bg-red-500 rounded-md px-3 py-1 mt-2"
+            >
+              Delete
+            </button>
+
+            <button
+              onClick={() => {
+                updateNote(note._id);
+              }}
+              className="bg-blue-500 rounded-md px-3 py-1 mt-2 ml-4"
+            >
+              Update
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
